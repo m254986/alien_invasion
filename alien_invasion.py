0240c8b8eb11159d932652bbb52bcad080283cfb
m254986 <115638575+m254986@users.noreply.github.com>
2022-10-26 09:46:26 -0400

    lab grind pt idk

--- alien_invasion.py
+++ alien_invasion.py
@@ -30,7 +30,7 @@ class AlienInvasion:
             # Redraw screen during each pass through the loop.
             self._check_events()
             self.ship.update()
-            self._update_sreen()
+            self._update_screen()
 
             # Watch for keyboard and mouse events!!
             for event in pygame.event.get():
@@ -42,18 +42,27 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    # Move ship right!!
-                    #self.ship.rect.x += 1
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
-
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Respond to keypresses."""
+        if event.key == pygame.K_RIGHT:
+            # Move ship right!!
+            # self.ship.rect.x += 1
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """Respond to key releases."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     def _update_screen(self):
         """Update images on screen then flip them!!"""
